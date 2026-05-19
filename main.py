68521118e353da8601845eb8220537bab7c34a88
VARSHA T <suryavarsha367@gmail.com>
2026-05-19 22:00:35 +0530

    Final working K-means clustering project

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-main.py
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+# Load dataset
+df = pd.read_csv("Mall_Customers.csv")
+
+print("First 5 rows:\n", df.head())
+
+# Select features
+X = df.iloc[:, [3, 4]].values
+
+# Elbow Method
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, random_state=42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1, 11), wcss, marker='o')
+plt.title("Elbow Method")
+plt.xlabel("Clusters")
+plt.ylabel("WCSS")
+plt.show()
+
+# KMeans model
+kmeans = KMeans(n_clusters=5, random_state=42)
+y_kmeans = kmeans.fit_predict(X)
+
+
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], label='Cluster 1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], label='Cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], label='Cluster 3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], label='Cluster 4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], label='Cluster 5')
+
+
+plt.scatter(kmeans.cluster_centers_[:, 0],
+            kmeans.cluster_centers_[:, 1],
+            s=200, c='yellow', label='Centroids')
+
+plt.title("Customer Segments")
+plt.xlabel("Income")
+plt.ylabel("Spending Score")
+plt.legend()
+plt.show()
